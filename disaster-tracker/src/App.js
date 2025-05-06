bdcad9c71cd032361e9cb0375d7502ce222e08d7
accuracies160 <andrew.page.160@my.csun.edu>
2025-05-05 23:35:08 -0700

    updated App.js

--- disaster-tracker/src/App.js
+++ disaster-tracker/src/App.js
@@ -1,18 +1,17 @@
-import React, { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import './App.css';
-import Map from './components/Map';
-import Filter from './components/Filter';
-import DisasterFilter from './components/DisasterFilter';
-import AuthButtons from './components/AuthButtons';
-import NotificationService from './components/NotificationService';
-import Noticeboard from './components/Noticeboard';
-import Navbar from './components/Navbar';
-import AccountPage from './components/AccountPage'; 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Map from "./components/Map";
+import Filter from "./components/Filter";
+import DisasterFilter from "./components/DisasterFilter";
+import AuthButtons from "./components/AuthButtons";
+import Noticeboard from "./components/Noticeboard";
+import Navbar from "./components/Navbar";
+import AccountPage from "./components/AccountPage";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  const [magnitudeFilter, setMagnitudeFilter] = useState('0');
+  const [magnitudeFilter, setMagnitudeFilter] = useState("0");
   const [disasterFilter, setDisasterFilter] = useState({
     earthquake: true,
     hurricane: true,
@@ -27,44 +26,60 @@ function App() {
     setDisasterFilter(newFilter);
   };
 
-   return (
+  return (
     <div className="App">
       <Navbar />
-      <div style={{ 
-        position: 'fixed', 
-        top: '20px', 
-        right: '20px', 
-        zIndex: 1001 
-      }}>
+      <div
+        style={{
+          position: "fixed",
+          top: "20px",
+          right: "20px",
+          zIndex: 1001,
+        }}
+      >
         <AuthButtons />
       </div>
-      
-      <div style={{ 
-        paddingTop: '80px', 
-        minHeight: 'calc(100vh - 80px)'
-      }}>
+
+      <div
+        style={{
+          paddingTop: "80px",
+          minHeight: "calc(100vh - 80px)",
+        }}
+      >
         <Routes>
-          <Route path="/" element={
-            <>
-              <h1>Natural Disaster Tracker</h1>
-              <Filter onFilterChange={handleFilterChange} />
-              <DisasterFilter
-                disasterFilter={disasterFilter}
-                onFilterChange={handleDisasterFilterChange}
-              />
-              <Map magnitudeFilter={magnitudeFilter} disasterFilter={disasterFilter} />
-            </>
-          } />
-          <Route path="/noticeboard" element={
-            <div style={{ padding: '20px' }}>
-              <Noticeboard />
-            </div>
-          } />
-          <Route path="/account" element={
-            <div style={{ padding: '20px' }}>
-              <AccountPage />
-            </div>
-          } />
+          <Route
+            path="/"
+            element={
+              <>
+                <h1>Natural Disaster Tracker</h1>
+                <Filter onFilterChange={handleFilterChange} />
+                <DisasterFilter
+                  disasterFilter={disasterFilter}
+                  onFilterChange={handleDisasterFilterChange}
+                />
+                <Map
+                  magnitudeFilter={magnitudeFilter}
+                  disasterFilter={disasterFilter}
+                />
+              </>
+            }
+          />
+          <Route
+            path="/noticeboard"
+            element={
+              <div style={{ padding: "20px" }}>
+                <Noticeboard />
+              </div>
+            }
+          />
+          <Route
+            path="/account"
+            element={
+              <div style={{ padding: "20px" }}>
+                <AccountPage />
+              </div>
+            }
+          />
         </Routes>
       </div>
     </div>
